4ab6a2c26de1803f39117b1bfa18c189a1858c82
Monica Djunaidi <modjun12@gmail.com>
2020-07-04 13:39:23 -0500

    created ui folder

---
+++ tastebuds.ui/src/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn btn-danger">poo</button>
+      </div>
+    );
+  }
+}
+
+export default App;
