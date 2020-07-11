56ae63efae775aa2c48d399717237ea1eea58101
Monica Djunaidi <modjun12@gmail.com>
2020-07-11 14:16:43 -0500

    can view single restaurant and go back to restaurant form with previous results shown again

--- tastebuds.ui/src/components/pages/SingleRestaurant/SingleRestaurant.js
+++ tastebuds.ui/src/components/pages/SingleRestaurant/SingleRestaurant.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import './SingleRestaurant.scss';
+import Card from 'react-bootstrap/Card';
 import restaurantData from '../../../helpers/data/restaurantData';
 
 class SingleRestaurant extends Component {
   state = {
     restaurant: {},
+    location: {},
+    highlights: [],
   }
 
   componentDidMount() {
     const { restaurantId } = this.props.match.params;
     restaurantData.getRestaurant(restaurantId)
-      .then((restaurant) => this.setState({ restaurant }))
+      .then((restaurant) => this.setState({ restaurant, location: restaurant.location, highlights: restaurant.highlights }))
       .catch((error) => console.error(error, 'errFromSingleRestaurant'));
   }
 
   render() {
+    const { restaurant, highlights, location } = this.state;
+
     return (
       <div className="SingleRestaurant">
-        <h1>Single Restaurant</h1>
+        <h1>{restaurant.name}</h1>
+        <Card>
+          <Card.Img variant="top" src={restaurant.featured_image}/>
+          <Card.Body>
+            <Card.Text>
+              Hours: {restaurant.timings}
+            </Card.Text>
+              Highlights:
+              {
+                highlights.map((highlight) => <Card.Text>{highlight}</Card.Text>)
+              }
+            <Card.Text>
+              Phone Number: {restaurant.phone_numbers}
+            </Card.Text>
+             <Card.Text>
+              Website: {restaurant.url}
+            </Card.Text>
+            <Card.Text>
+              Location: {location.address}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
