6b28084bef5d8539cbb2217b40960711bca208cb
Monica Djunaidi <modjun12@gmail.com>
2020-07-15 21:03:43 -0500

    added carousel and images

--- tastebuds.ui/src/components/pages/Home/Home.js
+++ tastebuds.ui/src/components/pages/Home/Home.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './Home.scss';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
+        <div className="carousel">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                alt="food-on-table"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1564486054184-1c26aa52b1c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                alt="front-restaurant-pic"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://images.unsplash.com/photo-1498654896293-37aacf113fd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                alt="korean-food"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </div>
     );
   }
