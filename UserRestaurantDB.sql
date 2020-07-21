535c98bd4f72b9819239185836a7f1bb9d08e6db
Monica Djunaidi <modjun12@gmail.com>
2020-07-20 19:51:49 -0500

    updated db

--- UserRestaurantDB.sql
+++ UserRestaurantDB.sql
@@ -3,3 +3,14 @@ create Table UserRestaurant (
 	UserId int foreign key references [User](UserId) not null,
 	RestaurantId int foreign key references Restaurant(RestaurantId) not null,
 )
+
+insert into UserRestaurant (UserId, RestaurantId)
+values (2, 17838043),
+		(2, 6401481),
+		(3, 17838043),
+		(3, 6400282),
+		(4, 17194668),
+		(4, 6401481),
+		(4, 17955135),
+		(5, 16728262),
+		(5, 17194353)
